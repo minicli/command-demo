96a0e8aa7d8eb4273a4d3fe6fa1fd6fc4cea6df6
Erika Heidi <erika@chainguard.dev>
2022-06-01 16:17:51 +0200

    Initial version

---
+++ src/Demo/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Minicli\Demo;
+
+use Minicli\Command\CommandController;
+
+class DefaultController extends CommandController
+{
+    public function handle(): void
+    {
+        $name = $this->hasParam('user') ? $this->getParam('user') : 'World';
+        $this->getPrinter()->display(sprintf("Hello, %s!", $name));
+
+        print_r($this->getParams());
+    }
+}
